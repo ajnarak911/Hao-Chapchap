8de6a788de820a2ff1a955f9ede70dec3108e5e8
ajnarak911 <ajnarakdaggy@gmail.com>
2024-04-30 23:43:26 +0300

    Complete creating of listing page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -4,6 +4,7 @@ import { getDownloadURL, getStorage, uploadBytesResumable} from 'firebase/storag
 import { app } from '../firebase';
 import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserStart, deleteUserFailure, deleteUserSuccess, signOutUserStart } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 export default function Profile() {
   const fileRef = useRef(null);
@@ -134,6 +135,12 @@ const handleSignOut = async () => {
         id='username' className='border p-3 rounded-lg' onChange={handleChange} />
         <button disabled={loading} className='bg-slate-500 text-white rounded-lg p-3 uppercase hover:opacity-90
          disabled:70'>{loading ? 'Loading...' : 'Update'}</button>
+          <Link
+          className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95'
+          to={'/create-listing'}
+        >
+          Create Listing
+        </Link>
 
       </form>
       <div className="flex justify-between mt-5">
