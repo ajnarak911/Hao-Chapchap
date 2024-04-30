baada7f1feab0476942f5aaaaad28fa12cc7d926
ajnarak911 <ajnarakdaggy@gmail.com>
2024-04-30 22:35:33 +0300

    creating a listing api route

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -25,9 +25,6 @@ export default function Header() {
             <Link to='/About'>
             <li className='text-slate-700 hover:underline'>About</li>
             </Link>
-            <Link to='/sign-in'>
-            <li className='text-slate-700 hover:underline'>Sign-In</li>
-            </Link>
             <Link to='/profile'>
                 {currentUser ? (
                     <img className='rounded-full h-7 w-7 object-cover' src={currentUser.avatar} alt='profile'/>
